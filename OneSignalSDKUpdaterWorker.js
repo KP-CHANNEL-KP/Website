b0be66700cba240b15de695a7a59b8bfc624987d
KP-CHANNEL-KP <kopaing232003@gmail.com>
2025-11-16 06:15:09 +0630

    Update and rename OneSignalSDKUpdaterWorker.js to ._OneSignalSDKWorker.js

--- OneSignalSDKUpdaterWorker.js
+++
@@ -1,7 +0,0 @@
-/**
- * OneSignal Service Worker Updater Default File
- * ဤဖိုင်ကို သင့်ဝက်ဘ်ဆိုက်၏ Root Directory တွင် ထည့်သွင်းရပါမည်။
- * ၎င်းသည် Notification စနစ်ကို အသစ်ပြင်ဆင်ရန် (Update) လုပ်ဆောင်ပေးသည်။
- */
-
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.js');
