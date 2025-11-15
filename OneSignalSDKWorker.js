209447a12989647acb251cc89bcfb54b8c845608
KP-CHANNEL-KP <kopaing232003@gmail.com>
2025-11-16 06:13:42 +0630

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,7 +1,1 @@
-/**
- * OneSignal Service Worker Default File
- * ဤဖိုင်ကို သင့်ဝက်ဘ်ဆိုက်၏ Root Directory တွင် ထည့်သွင်းရပါမည်။
- * ၎င်းသည် Notification များကို နောက်ခံ (Background) မှ လက်ခံရရှိစေရန် လုပ်ဆောင်ပေးသည်။
- */
-
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.js');
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
